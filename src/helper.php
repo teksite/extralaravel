78a50e6031764ee770e662ba9cf99473cb533e57
teksite <teksite.design@gmail.com>
2025-03-03 08:44:51 +0330

    add normalizenormalize function in helper file

--- src/helper.php
+++ src/helper.php
@@ -150,3 +150,18 @@ if (!function_exists('dotToArray')) {
             : $name;
     }
 }
+
+if (!function_exists('normalizePath')) {
+    /**
+     * @param string $path
+     * @return string
+     */
+    function normalizePath(string $path): string
+    {
+        // Replace all "/" and "\" with DIRECTORY_SEPARATOR
+        $normalizedPath = str_replace(['/', '\\', '/\\', '\\/'], DIRECTORY_SEPARATOR, $path);
+
+        // Ensure the path ends with DIRECTORY_SEPARATOR
+        return rtrim($normalizedPath, DIRECTORY_SEPARATOR);
+    }
+}
