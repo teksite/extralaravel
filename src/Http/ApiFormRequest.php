aa8bcc2a2fa6471f533fb9e9f10460f755fec0a8
teksite <teksite.design@gmail.com>
2025-07-03 13:18:51 +0330

    fix some bug

--- src/Http/ApiFormRequest.php
+++ src/Http/ApiFormRequest.php
@@ -5,6 +5,7 @@ namespace Teksite\Extralaravel\Http;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Teksite\Lareon\Enums\ResponseType;
 
 class ApiFormRequest extends FormRequest
 {
@@ -12,8 +13,9 @@ class ApiFormRequest extends FormRequest
     public function failedValidation(Validator $validator)
     {
         return throw new HttpResponseException(response()->json([
-            'messages' => $validator->errors(),
+            'errors' => $validator->errors(),
             'status' => 422,
+            'result' => ResponseType::FAILED,
             'data' => [],
         ])->setStatusCode(422));
     }
@@ -22,8 +24,9 @@ class ApiFormRequest extends FormRequest
     public function failedAuthorization()
     {
         return throw new HttpResponseException(response()->json([
-            'messages' => ["Forbidden You don't have permission"],
+            'errors' => ["Forbidden You don't have permission"],
             'status' => 403,
+            'result' => ResponseType::FAILED,
             'data' => [],
         ])->setStatusCode(403));
     }
