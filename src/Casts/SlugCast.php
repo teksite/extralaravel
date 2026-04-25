656628bc30e16d63b60162e8d1884ed0f38cb273
teksite <teksite.design@gmail.com>
2026-04-25 19:52:33 +0330

    delete JsonCast

--- src/Casts/SlugCast.php
+++ src/Casts/SlugCast.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class SlugCast implements CastsAttributes
 {
-    protected $slugs=[];
+    protected array $slugs=[];
 
     public function __construct(public ?string $routeName =null , ...$slugs)
     {
