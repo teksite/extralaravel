b5c05cb9e48398c69e03b984ae8123fad58d4f84
teksite <teksite.design@gmail.com>
2026-04-25 19:56:46 +0330

    fix issue HoneyPostMiddleware

--- src/Middleware/HoneypotMiddleware.php
+++ src/Middleware/HoneypotMiddleware.php
@@ -10,12 +10,9 @@ class HoneypotMiddleware
 
     public function handle(Request $request, Closure $next)
     {
-        $honeyField=config('extralaravel.honeypot.field_name' ?? 'data_info.fullname');
-        if ($request->has($honeyField) && !empty($request->input($honeyField))) {
-            // Block the request if the honeypot field is not empty.
-            return response('Forbidden', 403);
-        }
-
+        $honeyField=config('extralaravel.honeypot.field_name' ?? 'formpot');
+        if($request->input($honeyField)) abort(403);
+        
         return $next($request);
     }
 
