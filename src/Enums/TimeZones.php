12bf4b9db98d9bbc1736c5196810dade5a4cfc36
teksite <teksite.design@gmail.com>
2026-04-26 11:52:16 +0330

    refactor TimeZone Enum

--- src/Enums/TimeZones.php
+++ src/Enums/TimeZones.php
@@ -2,9 +2,411 @@
 
 namespace Teksite\Extralaravel\Enums;
 
-enum TimeZones: string
+enum TimeZones
 {
-    const array times = [
+    case Asia_Tehran;
+    case America_Adak;
+    case America_Atka;
+    case America_Anchorage;
+    case America_Juneau;
+    case America_Nome;
+    case America_Yakutat;
+    case America_Dawson;
+    case America_Ensenada;
+    case America_Los_Angeles;
+    case America_Tijuana;
+    case America_Vancouver;
+    case America_Whitehorse;
+    case Canada_Pacific;
+    case Canada_Yukon;
+    case Mexico_BajaNorte;
+    case America_Boise;
+    case America_Cambridge_Bay;
+    case America_Chihuahua;
+    case America_Dawson_Creek;
+    case America_Denver;
+    case America_Edmonton;
+    case America_Hermosillo;
+    case America_Inuvik;
+    case America_Mazatlan;
+    case America_Phoenix;
+    case America_Shiprock;
+    case America_Yellowknife;
+    case Canada_Mountain;
+    case Mexico_BajaSur;
+    case America_Belize;
+    case America_Cancun;
+    case America_Chicago;
+    case America_Costa_Rica;
+    case America_El_Salvador;
+    case America_Guatemala;
+    case America_Knox_IN;
+    case America_Managua;
+    case America_Menominee;
+    case America_Merida;
+    case America_Mexico_City;
+    case America_Monterrey;
+    case America_Rainy_River;
+    case America_Rankin_Inlet;
+    case America_Regina;
+    case America_Swift_Current;
+    case America_Tegucigalpa;
+    case America_Winnipeg;
+    case Canada_Central;
+    case Canada_East_Saskatchewa;
+    case Canada_Saskatchewan;
+    case Chile_EasterIsland;
+    case Mexico_General;
+    case America_Atikokan;
+    case America_Bogota;
+    case America_Cayman;
+    case America_Coral_Harbour;
+    case America_Detroit;
+    case America_Fort_Wayne;
+    case America_Grand_Turk;
+    case America_Guayaquil;
+    case America_Havana;
+    case America_Indianapolis;
+    case America_Iqaluit;
+    case America_Jamaica;
+    case America_Lima;
+    case America_Louisville;
+    case America_Montreal;
+    case America_Nassau;
+    case America_New_York;
+    case America_Nipigon;
+    case America_Panama;
+    case America_Pangnirtung;
+    case America_Port_au_Prince;
+    case America_Resolute;
+    case America_Thunder_Bay;
+    case America_Toronto;
+    case Canada_Eastern;
+    case America_Caracas;
+    case America_Anguilla;
+    case America_Antigua;
+    case America_Aruba;
+    case America_Asuncion;
+    case America_Barbados;
+    case America_Blanc_Sablon;
+    case America_Boa_Vista;
+    case America_Campo_Grande;
+    case America_Cuiaba;
+    case America_Curacao;
+    case America_Dominica;
+    case America_Eirunepe;
+    case America_Glace_Bay;
+    case America_Goose_Bay;
+    case America_Grenada;
+    case America_Guadeloupe;
+    case America_Guyana;
+    case America_Halifax;
+    case America_La_Paz;
+    case America_Manaus;
+    case America_Marigot;
+    case America_Martinique;
+    case America_Moncton;
+    case America_Montserrat;
+    case America_Port_of_Spain;
+    case America_Porto_Acre;
+    case America_Porto_Velho;
+    case America_Puerto_Rico;
+    case America_Rio_Branco;
+    case America_Santiago;
+    case America_Santo_Domingo;
+    case America_St_Barthelemy;
+    case America_St_Kitts;
+    case America_St_Lucia;
+    case America_St_Thomas;
+    case America_St_Vincent;
+    case America_Thule;
+    case America_Tortola;
+    case America_Virgin;
+    case Antarctica_Palmer;
+    case Atlantic_Bermuda;
+    case Atlantic_Stanley;
+    case Brazil_Acre;
+    case Brazil_West;
+    case Canada_Atlantic;
+    case Chile_Continental;
+    case America_St_Johns;
+    case Canada_Newfoundland;
+    case America_Araguaina;
+    case America_Bahia;
+    case America_Belem;
+    case America_Buenos_Aires;
+    case America_Catamarca;
+    case America_Cayenne;
+    case America_Cordoba;
+    case America_Fortaleza;
+    case America_Godthab;
+    case America_Jujuy;
+    case America_Maceio;
+    case America_Mendoza;
+    case America_Miquelon;
+    case America_Montevideo;
+    case America_Paramaribo;
+    case America_Recife;
+    case America_Rosario;
+    case America_Santarem;
+    case America_Sao_Paulo;
+    case Antarctica_Rothera;
+    case Brazil_East;
+    case America_Noronha;
+    case Atlantic_South_Georgia;
+    case Brazil_DeNoronha;
+    case America_Scoresbysund;
+    case Atlantic_Azores;
+    case Atlantic_Cape_Verde;
+    case Africa_Abidjan;
+    case Africa_Accra;
+    case Africa_Bamako;
+    case Africa_Banjul;
+    case Africa_Bissau;
+    case Africa_Casablanca;
+    case Africa_Conakry;
+    case Africa_Dakar;
+    case Africa_El_Aaiun;
+    case Africa_Freetown;
+    case Africa_Lome;
+    case Africa_Monrovia;
+    case Africa_Nouakchott;
+    case Africa_Ouagadougou;
+    case Africa_Sao_Tome;
+    case Africa_Timbuktu;
+    case America_Danmarkshavn;
+    case Atlantic_Canary;
+    case Atlantic_Faeroe;
+    case Atlantic_Faroe;
+    case Atlantic_Madeira;
+    case Atlantic_Reykjavik;
+    case Atlantic_St_Helena;
+    case Europe_Belfast;
+    case Europe_Dublin;
+    case Europe_Guernsey;
+    case Europe_Isle_of_Man;
+    case Europe_Jersey;
+    case Europe_Lisbon;
+    case Europe_London;
+    case Africa_Algiers;
+    case Africa_Bangui;
+    case Africa_Brazzaville;
+    case Africa_Ceuta;
+    case Africa_Douala;
+    case Africa_Kinshasa;
+    case Africa_Lagos;
+    case Africa_Libreville;
+    case Africa_Luanda;
+    case Africa_Malabo;
+    case Africa_Ndjamena;
+    case Africa_Niamey;
+    case Africa_Porto_Novo;
+    case Africa_Tunis;
+    case Africa_Windhoek;
+    case Arctic_Longyearbyen;
+    case Atlantic_Jan_Mayen;
+    case Europe_Amsterdam;
+    case Europe_Andorra;
+    case Europe_Belgrade;
+    case Europe_Berlin;
+    case Europe_Bratislava;
+    case Europe_Brussels;
+    case Europe_Budapest;
+    case Europe_Copenhagen;
+    case Europe_Gibraltar;
+    case Europe_Ljubljana;
+    case Europe_Luxembourg;
+    case Europe_Madrid;
+    case Europe_Malta;
+    case Europe_Monaco;
+    case Europe_Oslo;
+    case Europe_Paris;
+    case Europe_Podgorica;
+    case Europe_Prague;
+    case Europe_Rome;
+    case Europe_San_Marino;
+    case Europe_Sarajevo;
+    case Europe_Skopje;
+    case Europe_Stockholm;
+    case Europe_Tirane;
+    case Europe_Vaduz;
+    case Europe_Vatican;
+    case Europe_Vienna;
+    case Europe_Warsaw;
+    case Europe_Zagreb;
+    case Europe_Zurich;
+    case Africa_Blantyre;
+    case Africa_Bujumbura;
+    case Africa_Cairo;
+    case Africa_Gaborone;
+    case Africa_Harare;
+    case Africa_Johannesburg;
+    case Africa_Kigali;
+    case Africa_Lubumbashi;
+    case Africa_Lusaka;
+    case Africa_Maputo;
+    case Africa_Maseru;
+    case Africa_Mbabane;
+    case Africa_Tripoli;
+    case Asia_Amman;
+    case Asia_Beirut;
+    case Asia_Damascus;
+    case Asia_Gaza;
+    case Asia_Istanbul;
+    case Asia_Jerusalem;
+    case Asia_Nicosia;
+    case Asia_Tel_Aviv;
+    case Europe_Athens;
+    case Europe_Bucharest;
+    case Europe_Chisinau;
+    case Europe_Helsinki;
+    case Europe_Istanbul;
+    case Europe_Kaliningrad;
+    case Europe_Kiev;
+    case Europe_Mariehamn;
+    case Europe_Minsk;
+    case Europe_Nicosia;
+    case Europe_Riga;
+    case Europe_Simferopol;
+    case Europe_Sofia;
+    case Europe_Tallinn;
+    case Europe_Tiraspol;
+    case Europe_Uzhgorod;
+    case Europe_Vilnius;
+    case Europe_Zaporozhye;
+    case Africa_Addis_Ababa;
+    case Africa_Asmara;
+    case Africa_Asmera;
+    case Africa_Dar_es_Salaam;
+    case Africa_Djibouti;
+    case Africa_Kampala;
+    case Africa_Khartoum;
+    case Africa_Mogadishu;
+    case Africa_Nairobi;
+    case Antarctica_Syowa;
+    case Asia_Aden;
+    case Asia_Baghdad;
+    case Asia_Bahrain;
+    case Asia_Kuwait;
+    case Asia_Qatar;
+    case Europe_Moscow;
+    case Europe_Volgograd;
+    case Indian_Antananarivo;
+    case Indian_Comoro;
+    case Indian_Mayotte;
+    case Asia_Baku;
+    case Asia_Dubai;
+    case Asia_Muscat;
+    case Asia_Tbilisi;
+    case Asia_Yerevan;
+    case Europe_Samara;
+    case Indian_Mahe;
+    case Indian_Mauritius;
+    case Indian_Reunion;
+    case Asia_Kabul;
+    case Asia_Aqtau;
+    case Asia_Aqtobe;
+    case Asia_Ashgabat;
+    case Asia_Ashkhabad;
+    case Asia_Dushanbe;
+    case Asia_Karachi;
+    case Asia_Oral;
+    case Asia_Samarkand;
+    case Asia_Tashkent;
+    case Asia_Yekaterinburg;
+    case Indian_Kerguelen;
+    case Indian_Maldives;
+    case Asia_Calcutta;
+    case Asia_Colombo;
+    case Asia_Kolkata;
+    case Asia_Katmandu;
+    case Antarctica_Mawson;
+    case Antarctica_Vostok;
+    case Asia_Almaty;
+    case Asia_Bishkek;
+    case Asia_Dacca;
+    case Asia_Dhaka;
+    case Asia_Novosibirsk;
+    case Asia_Omsk;
+    case Asia_Qyzylorda;
+    case Asia_Thimbu;
+    case Asia_Thimphu;
+    case Indian_Chagos;
+    case Asia_Rangoon;
+    case Indian_Cocos;
+    case Antarctica_Davis;
+    case Asia_Bangkok;
+    case Asia_Ho_Chi_Minh;
+    case Asia_Hovd;
+    case Asia_Jakarta;
+    case Asia_Krasnoyarsk;
+    case Asia_Phnom_Penh;
+    case Asia_Pontianak;
+    case Asia_Saigon;
+    case Asia_Vientiane;
+    case Indian_Christmas;
+    case Antarctica_Casey;
+    case Asia_Brunei;
+    case Asia_Choibalsan;
+    case Asia_Chongqing;
+    case Asia_Chungking;
+    case Asia_Harbin;
+    case Asia_Hong_Kong;
+    case Asia_Irkutsk;
+    case Asia_Kashgar;
+    case Asia_Kuala_Lumpur;
+    case Asia_Kuching;
+    case Asia_Macao;
+    case Asia_Macau;
+    case Asia_Makassar;
+    case Asia_Manila;
+    case Asia_Shanghai;
+    case Asia_Singapore;
+    case Asia_Taipei;
+    case Asia_Ujung_Pandang;
+    case Asia_Ulaanbaatar;
+    case Asia_Ulan_Bator;
+    case Asia_Urumqi;
+    case Australia_Perth;
+    case Australia_West;
+    case Australia_Eucla;
+    case Asia_Dili;
+    case Asia_Jayapura;
+    case Asia_Pyongyang;
+    case Asia_Seoul;
+    case Asia_Tokyo;
+    case Asia_Yakutsk;
+    case Australia_Adelaide;
+    case Australia_Broken_Hill;
+    case Australia_Darwin;
+    case Australia_North;
+    case Australia_South;
+    case Australia_Yancowinna;
+    case Antarctica_DumontDUrvil;
+    case Asia_Sakhalin;
+    case Asia_Vladivostok;
+    case Australia_ACT;
+    case Australia_Brisbane;
+    case Australia_Canberra;
+    case Australia_Currie;
+    case Australia_Hobart;
+    case Australia_Lindeman;
+    case Australia_Melbourne;
+    case Australia_NSW;
+    case Australia_Queensland;
+    case Australia_Sydney;
+    case Australia_Tasmania;
+    case Australia_Victoria;
+    case Australia_LHI;
+    case Australia_Lord_Howe;
+    case Asia_Magadan;
+    case Antarctica_McMurdo;
+    case Antarctica_South_Pole;
+    case Asia_Anadyr;
+    case Asia_Kamchatka;
+
+
+    const array TIMES = [
         'Asia/Tehran'               => '+3:30',
         'America/Adak'              => '-10:00',
         'America/Atka'              => '-10:00',
@@ -54,7 +456,7 @@ enum TimeZones: string
         'America/Tegucigalpa'       => '-6:00',
         'America/Winnipeg'          => '-6:00',
         'Canada/Central'            => '-6:00',
-        'Canada/East-Saskatchewan'  => '-6:00',
+        'Canada/East_Saskatchewan'  => '-6:00',
         'Canada/Saskatchewan'       => '-6:00',
         'Chile/EasterIsland'        => '-6:00',
         'Mexico/General'            => '-6:00',
@@ -78,7 +480,7 @@ enum TimeZones: string
         'America/Nipigon'           => '-5:00',
         'America/Panama'            => '-5:00',
         'America/Pangnirtung'       => '-5:00',
-        'America/Port-au-Prince'    => '-5:00',
+        'America/Port_au_Prince'    => '-5:00',
         'America/Resolute'          => '-5:00',
         'America/Thunder_Bay'       => '-5:00',
         'America/Toronto'           => '-5:00',
@@ -89,7 +491,7 @@ enum TimeZones: string
         'America/Aruba'             => '-4:00',
         'America/Asuncion'          => '-4:00',
         'America/Barbados'          => '-4:00',
-        'America/Blanc-Sablon'      => '-4:00',
+        'America/Blanc_Sablon'      => '-4:00',
         'America/Boa_Vista'         => '-4:00',
         'America/Campo_Grande'      => '-4:00',
         'America/Cuiaba'            => '-4:00',
@@ -201,7 +603,7 @@ enum TimeZones: string
         'Africa/Malabo'             => '+1:00',
         'Africa/Ndjamena'           => '+1:00',
         'Africa/Niamey'             => '+1:00',
-        'Africa/Porto-Novo'         => '+1:00',
+        'Africa/Porto_Novo'         => '+1:00',
         'Africa/Tunis'              => '+1:00',
         'Africa/Windhoek'           => '+1:00',
         'Arctic/Longyearbyen'       => '+1:00',
@@ -407,6 +809,53 @@ enum TimeZones: string
         'Asia/Kamchatka'            => '+12:00',
     ];
 
+    /**
+     * @param string $zone
+     * @return string
+     */
+    private static function toSlash(string $zone): string
+    {
+        return preg_replace('/_/', '/', $zone, 1);
+    }
+
+    /**
+     * @param string $zone
+     * @return string
+     */
+    private static function toUnderscore(string $zone): string
+    {
+        return str_replace('/', '_', $zone);
+    }
+
+    /**
+     * @return array
+     */
+    public static function allZones(): array
+    {
+        return array_keys(self::TIMES);
+    }
+
+    /**
+     * @return array
+     */
+    public static function allTimes(): array
+    {
+        return array_unique(array_values(self::TIMES));
+    }
+
+
+    /**
+     * @param string|TimeZones $zone
+     * @return string|null
+     */
+    public static function getOffset(string|self $zone): ?string
+    {
+        $key = $zone instanceof self
+            ? self::toSlash($zone->name)
+            : (str_contains($zone, '/') ? $zone : self::toSlash($zone));
+
+        return self::TIMES[$key] ?? null;
+    }
 
 
 }
